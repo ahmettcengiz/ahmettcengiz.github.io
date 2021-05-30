7545af459e4593fcb4a4b279cc45939cf736d050
AhmetHamdiCengiz <66183595+AhmetHamdiCengiz@users.noreply.github.com>
2021-05-30 21:46:57 +0300

    Update

---
+++ InClassActivities/Cengiz_Activity10.js
@@ -0,0 +1,16 @@
+$(function(){
+    $("#birthday").datepicker();
+});
+var languages = [
+    "Kotlin", 
+    "Java", 
+    "Dart",
+    "C++",
+    "GoLang",
+    "SQL",
+    "C#",
+    "Python"
+    ];
+$( "#languages" ).autocomplete({
+    source: languages 
+    });
