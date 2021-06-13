874ddc4f6844b6cc9ca3eac000999c4940877fbe
AhmetHamdiCengiz <66183595+AhmetHamdiCengiz@users.noreply.github.com>
2021-06-13 20:34:49 +0300

    update

--- WebProject4/js/news.js
+++ WebProject4/js/news.js
@@ -1,10 +1,11 @@
 $.ajax({
-    url:"https://api.flickr.com/services/feeds/photos_public.gne?%22%20+%22id=82407828@N07&format=json&jsoncallback=?&tags=movie",
+    url:"https://api.flickr.com/services/feeds/photos_public.gne?%22%20+%22id=82407828@N07&format=json&jsoncallback=?&tags=film",
     dataType:"json",
     type:"GET",
     success:function (data){
         for (let i=0;i<8;i++){
             $("#imgitem"+i+"").attr("src",data["items"][i].media.m);
+			$("#pitem"+i+"").append("",data["items"][i].tags);
 	
            console.log(data["items"][i].media.m);
         }
